4acd9de1c1fe33161e75573247cb8381e5fd3d8e
brink.qiang <brink.qiang@gmail.com>
2018-09-22 10:41:37 +0800

    add

--- test/dmcsvtest.cpp
+++ test/dmcsvtest.cpp
@@ -10,7 +10,7 @@ int main()
 
     std::queue<std::vector<std::string>> q;
     q.push({ "name", "age", "ip" });
-    q.push({ "Tom", "23", "172.30.10.29" });
+    q.push({ "Tom", std::to_string(std::numeric_limits<uint64_t>::max()).c_str(), "172.30.10.29" });
     q.push({ "Plus", "22", "172.30.10.18" });
     q.push({ "Andy", "27", "172.30.10.21" });
 
@@ -27,7 +27,7 @@ int main()
 
         for (size_t i = 0; reader.read_row(rows); i++) {
             std::cout << rows["name"].get<std::string>() << "\t"
-                << rows["age"].get<std::string>() << "\t"
+                << rows["age"].get<uint64_t>() << "\t"
                 << rows["ip"].get<std::string>() << std::endl;
         }
     }
