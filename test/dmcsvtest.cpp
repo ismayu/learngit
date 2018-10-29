0c1363f7117adbf6edacfcd3c573e219f7017887
brinkqiang <brink.qiang@gmail.com>
2018-10-29 15:23:25 +0800

    add

--- test/dmcsvtest.cpp
+++ test/dmcsvtest.cpp
@@ -4,7 +4,6 @@
 #include "dmcsv_writer.hpp"
 #include "dmdsvfilter.hpp"
 
-uint64_t dmcsv_count = 0;
 std::string strFile = "slist.csv";
 std::queue<std::vector<std::string>> q;
 
@@ -12,12 +11,14 @@ TEST(dmcsv, dmcsv_init) {
     std::ofstream out(strFile);
     q.push({ "name", "age", "ip" });
 
+    uint64_t qwCount = 0;
+
     for (int i = 0; i < 1000000; ++i)
     {
-        dmcsv_count += i;
+        qwCount += i;
         q.push({ "Andy" + std::to_string(i), std::to_string(i), "172.30.10.21" });
     }
-
+    std::cout << qwCount << std::endl;
     out.close();
 }
 
@@ -33,13 +34,13 @@ TEST(dmcsv, dmcsv_write) {
 
 TEST(dmcsv, dmcsv_read) {
 
-    uint64_t qwCount = dmcsv_count;
+    uint64_t qwCount = 0;
 
     csv::CSVReader reader(strFile, csv::DEFAULT_CSV);
     csv::CSVRow rows;
     for (size_t i = 0; reader.read_row(rows); i++) {
         //qwCount -= rows["age"].get<uint64_t>();
-        qwCount -= rows[1].get<uint64_t>();
+        qwCount += rows[1].get<uint64_t>();
         //std::cout << rows["name"].get<std::string>() << "," << rows["age"].get<std::uint64_t>() << "," << rows["ip"].get<std::string>() << std::endl;
     }
     std::cout << qwCount << std::endl;
@@ -53,12 +54,12 @@ TEST(dsv_filter, dsv_filter_read) {
         return;
     }
 
-    uint64_t qwCount = dmcsv_count;
+    uint64_t qwCount = 0;
 
     for (int i = 1; i < static_cast<int>(oFilter.grid().row_count()); ++i)
     {
         strtk::token_grid::row_type row = oFilter.grid().row(i);
-        qwCount -= row.get<uint64_t>(1);
+        qwCount += row.get<uint64_t>(1);
     }
 
     std::cout << qwCount << std::endl;
